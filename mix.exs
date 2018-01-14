c749aa9fe443959e495f0beff0042bfde0511109
Eskil Heyn Olsen <eskil@eskil.org>
2018-01-14 13:34:41 -0800

    Update versions for ecto/comeonein/plug deps

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule Oauth2Server.Mixfile do
 
   def project do
     [app: :oauth2_server,
-     version: "0.1.1",
-     elixir: "~> 1.2",
+     version: "0.2.0",
+     elixir: "~> 1.5",
      description: "An elixir Oauth 2.0 server",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -23,10 +23,10 @@ defmodule Oauth2Server.Mixfile do
 
   defp deps do
     [
-      {:ecto, "~> 1.1.2"},
+      {:ecto, "~> 2.1"},
       {:poison, "~> 1.5 or ~> 2.0"},
-      {:comeonin, "~> 2.4"},
-      {:plug, "~> 1.1.6"}
+      {:comeonin, "~> 4.0"},
+      {:plug, "~> 1.4"}
     ]
   end
 
